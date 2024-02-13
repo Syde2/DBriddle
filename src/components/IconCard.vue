393b80ea0f7c4aea094bc40ae26d7d0d87a19c0b
Syde2 <contact@syde2hub.com>
2024-02-13 20:54:38 +0100

    Debut Page Index

---
+++ src/components/IconCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+
+
+
+</script>
+
+
+<template>
+	<div class="card">
+		<span class="material-symbols-outlined">search</span>
+	</div>
+</template>
+
+<style>
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 24
+}
+</style>
